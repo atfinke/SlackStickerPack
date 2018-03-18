ca7428f42ffef6fc19309b066efcb0a639127ca9
Andrew Finke <andrewfinke@me.com>
2018-03-17 20:42:43 -0500

    Init

---
+++ SlackAPI Demo/SlackAPI Demo/AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  SlackAPI Demo
+//
+//  Created by Andrew Finke on 3/17/18.
+//  Copyright © 2018 Andrew Finke. All rights reserved.
+//
+
+import UIKit
+import OAuthSwift
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        if (url.host == "oauth-callback") {
+            OAuthSwift.handle(url: url)
+        }
+        return true
+    }
+
+}
+
